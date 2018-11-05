5e27c200a346dddeb8a9aed15cc4f2cf945dbc5f
sflanders95 <sflanders95@WIn10Sm.localdomain>
2018-11-05 09:58:13 -0500

    removed a few files from archiving

--- pack.sh
+++ pack.sh
@@ -3,10 +3,10 @@
 FNAME="`date '+%Y%m%d_%H%M'`_WebAPI.tgz"
 EX="--exclude=WebAPI/obj "
 EX="${EX} --exclude=WebAPI/bin "
-EX="${EX} --exclude=WebAPI.Tests/bin "
-EX="${EX} --exclude=WebAPI.Tests/obj "
+#EX="${EX} --exclude=WebAPI.Tests/bin "
+#EX="${EX} --exclude=WebAPI.Tests/obj "
 
 set -x
-tar -czf $FNAME $EX .vs WebAPI WebAPI.Tests WebAPI.sln pack.sh
+tar -czf $FNAME $EX .vs WebAPI WebAPI.sln pack.sh browsing.sh .gitignore .gitattributes
 set +x
 
